a67fceaf3f33396b6d84319eec82b3b85b4a67c2
Onar Berrade <onarbf@gmail.com>
2023-07-26 12:29:11 +0200

    --

--- src/components/PortfolioSlider.tsx
+++ src/components/PortfolioSlider.tsx
@@ -10,7 +10,7 @@ export const PortfolioSlider = () => {
           <h1 className='text-5xl xl:text-8xl font-bold text-white' onClick={() => setWebsiteURL('https://www.todoist.com')}>Mayeutica.io</h1>
           <h2 className='mt-4 text-base xl:text-xl text-white'>A cool project based on AI. Each day, Mayeutica do a review of the most important topics of the day about Spain and then generates by itself an article. It also generates the sources of the news and images using MidJourney. A beautiful newspaper to read all news in one.</h2>
           <h3 className='mt-4 text-base  xl:text-lg font-semibold text-white'>Technologies:</h3>
-          <h2 className='text-base xl:text-xl text-white'>NextJS | Typescript | NodeJS | Strapi | React</h2>
+          <h2 className='text-base xl:text-xl text-white'>NextJS |Typescript |NodeJS |Strapi | React</h2>
         </section>
         <section className='flex flex-col justify-center  grow '>
           <div className=' grow max-h-[100%] md:max-h-[600px] xl:max-h-[800px]'>
