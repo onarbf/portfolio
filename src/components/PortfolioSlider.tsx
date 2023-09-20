33bcae30ec054616d4dce34fc052a92fb999f780
Onar Berrade <onarbf@gmail.com>
2023-09-20 09:56:05 +0200

    --

--- src/components/PortfolioSlider.tsx
+++ src/components/PortfolioSlider.tsx
@@ -31,7 +31,7 @@ export const PortfolioSlider = () => {
 
   return (
     <section id='projects' className='relative h-[900px] md:h-[800px] my-[48px] w-full'>
-      <div className='relative h-full z-0 w-full md:w-auto md:ml-[400px] '>
+      <div className='relative h-full ml-[0px] z-0 w-full md:w-auto md:ml-[400px] '>
         <MacModel websiteUrl={slides[slide].websiteUrl} isIframeLoaded={isIframeLoaded} handleIframeLoading={handleIframeLoading} />
       </div>
       <div className='relative md:w-[1112px]   md:mx-auto'>
